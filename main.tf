9da37bcef2b20cde0e15a720f4b927965e9808a5
Tacciana <123711868+lynxBios@users.noreply.github.com>
2024-10-21 00:49:32 +0300

    fix: file name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  access_key = variables.aws_access_key
+  secret_key = variables.aws_secret_key
+  region     = variables.aws_region
 }
 
 resource "aws_s3_bucket" "rs-school-bucket-1" {
