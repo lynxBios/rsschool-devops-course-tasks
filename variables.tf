b98902e69497f01d2e22201110f02dcc9bc17018
Tacciana <123711868+lynxBios@users.noreply.github.com>
2024-10-21 10:07:35 +0300

    fix: variables.tfvars

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "aws_access_key" {
+  type = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type = string
+  sensitive = true
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "githubActions_role_arn" {
+  type = string
+}
+
+variable "repository" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
