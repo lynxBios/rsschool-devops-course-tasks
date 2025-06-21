96287555d8b6efd3c374eeaea4a753a470d7fc97
Tacciana <123711868+lynxBios@users.noreply.github.com>
2025-06-21 09:54:58 +0300

    feat: implement base network infrastructure (VPC, subnets, IGW)

--- variables.tf
+++ variables.tf
@@ -1,8 +1,33 @@
-// variables.tf: все переменные для инфраструктуры
+// variables.tf: all infrastructure variables
+
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources in"
   type        = string
   default     = "eu-central-1"
+}
+
+variable "azs" {
+  description = "List of Availability Zones to use"
+  type        = list(string)
+  default     = ["eu-central-1a", "eu-central-1b"]
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "172.31.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["172.31.1.0/24", "172.31.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["172.31.101.0/24", "172.31.102.0/24"]
 }
 
 variable "project_name" {
